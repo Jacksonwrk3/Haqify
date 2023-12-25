19a2f46abe66ea23a879cf3b8630652672d94cee
Jacksonwrk3 <Jacksonwrk3@gmail.com>
2023-12-25 16:20:42 -0500

    Working on HeroBanner

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Navigation from "../components/Navigation/Navigation.jsx";
 import MenuModal from "../components/MenuModal/MenuModal.jsx";
+import HeroBanner from "../components/HeroBanner/HeroBanner.jsx";
 export default function Home() {
   return (
     <>
       <header className="relative z-50 w-screen ">
         <Navigation />
       </header>
-      <main></main>
+      <main>
+        <HeroBanner />
+      </main>
     </>
   );
 }
---
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const HeroBanner = () => {
+  return (
+    <section className="flex flex-col items-center h-screen border border-blue-300">
+      <p className="text-center bg-indigo-300 border border-indigo-300 rounded-lg w-72">
+        Over 3 million passion developer
+      </p>
+      <h1 className="text-4xl text-center">
+        Network and collaborate with passionate developers
+      </h1>
+    </section>
+  );
+};
+
+export default HeroBanner;
